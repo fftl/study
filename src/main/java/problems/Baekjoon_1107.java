64ec84b83d84b03e61bf0da0eb8ff870278d6609
fftl <fftl.0710@gmail.com>
2022-06-29 12:48:31 +0900

    Baekjoon 1107

--- src/main/java/problems/Baekjoon_1107.java
+++ src/main/java/problems/Baekjoon_1107.java
@@ -4,34 +4,58 @@ package problems;
 * 백준_1107_리모컨 G5
 * */
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-
-/*
+/* 입력예
 5457
 3
 6 7 8
  */
-/*
-0
+
+/* 출력예
+6
  */
+import java.util.Scanner;
+
 public class Baekjoon_1107 {
-    static int n;
-    static int[] button;
+    static int N, min, minNum;
+    static boolean[] button;
+
+    static void check(int num){
+        int result = Math.abs(N-num) + Integer.toString(num).length();
+        min = Math.min(result, min);
+        return;
+    }
+
+    static void createNum(String s){
+        if(Integer.parseInt(s) > 999999){
+            return;
+        } else if (s.equals("0")){
+            check(0);
+            return;
+        } else {
+            check(Integer.parseInt(s));
+            for(int i=0; i<10; i++){
+                if(!button[i]) createNum(s + Integer.toString(i));
+            }
+        }
+    }
+
     public static void main(String args[]){
-        n = 100;
 
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        N = sc.nextInt();
         int M = sc.nextInt();
 
-        button = new int[M];
-        for(int i=0; i<M; i++){
-            button[i] = sc.nextInt();
+        button = new boolean[10];
+        for(int i=0; i<M; i++) {
+            button[sc.nextInt()] = true;
         }
 
-        System.out.println(Arrays.toString(button));
+        min = Math.abs(N-100);
+        for(int i=0; i<10; i++){
+            if(!button[i]) createNum(Integer.toString(i));
+        }
+
+        System.out.println(min);
 
         sc.close();
     }
